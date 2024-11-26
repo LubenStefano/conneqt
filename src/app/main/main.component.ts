0f9574828fba9511383354d2d47ba820122a1422
LubenStefano <lubenstefanovkn@gmail.com>
2024-11-26 18:20:57 +0200

    refactor: move CreatePostAsideComponent to a new directory and update imports

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
-import { CreatePostAsideComponent } from './create-post-aside/create-post-aside.component';
+import { CreatePostAsideComponent } from '../create-post-aside/create-post-aside.component';
 import { SavedPostsAsideComponent } from './saved-posts-aside/saved-posts-aside.component';
 import { PostFlowComponent } from './post-flow/post-flow.component';
+import { UserBadgeComponent } from './user-badge/user-badge.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CreatePostAsideComponent, SavedPostsAsideComponent, PostFlowComponent],
+  imports: [
+    CreatePostAsideComponent,
+    SavedPostsAsideComponent,
+    PostFlowComponent,
+    UserBadgeComponent,
+  ],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+  styleUrl: './main.component.css',
 })
-export class MainComponent {
-
-}
+export class MainComponent {}
--- src/app/main/saved-posts-aside/saved-posts-aside.component.ts
+++ src/app/main/saved-posts-aside/saved-posts-aside.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import { UserBadgeComponent } from "../user-badge/user-badge.component";
+import { UserBadgeComponent } from '../user-badge/user-badge.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faXmarkCircle } from '@fortawesome/free-regular-svg-icons';
-import { faBookmark} from '@fortawesome/free-solid-svg-icons';
+import { faBookmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-saved-posts-aside',
   standalone: true,
   imports: [UserBadgeComponent, FontAwesomeModule],
   templateUrl: './saved-posts-aside.component.html',
-  styleUrl: './saved-posts-aside.component.css'
+  styleUrl: './saved-posts-aside.component.css',
 })
 export class SavedPostsAsideComponent {
   faBookmark = faBookmark;
