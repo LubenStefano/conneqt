2edb2d5a35d782fefa8719547415eee1f7f4663d
LubenStefano <lubenstefanovkn@gmail.com>
2024-12-10 17:01:31 +0200

    feat: enhance header component with menu close functionality; update styles for profile and post flow components for mobile

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,32 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { UserService } from '../../user/user.service';
 import { User } from '@angular/fire/auth';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [RouterLink],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
   user: User | null = null;
   isMenuOpen = false;
 
-  constructor(private userService: UserService, private router: Router) {}
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private renderer: Renderer2,
+    private elRef: ElementRef
+  ) {}
 
   ngOnInit() {
     this.userService.getUser().subscribe((user) => {
       this.user = user;
+    });
+
+    // Close menu on route change
+    this.router.events.subscribe(() => {
+      this.closeMenu();
+    });
+
+    // Close menu on clicking outside
+    this.renderer.listen('window', 'click', (event: Event) => {
+      if (
+        this.isMenuOpen && 
+        !this.elRef.nativeElement.contains(event.target)
+      ) {
+        this.closeMenu();
+      }
     });
   }
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
-  
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+
   onLogout() {
     this.userService.logout().subscribe({
       next: () => {
@@ -38,6 +61,7 @@ export class HeaderComponent implements OnInit {
       }
     });
   }
+
   scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
